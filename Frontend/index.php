d2edf0ee71cf0ee7de367831568e561001ce1344
Jesus Danilo Cervera Caceres <l20070013@valladolid.tecnm.mx>
2023-11-28 23:50:35 -0600

    lokitas

---
+++ Frontend/index.php
@@ -0,0 +1,45 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
+  <title>El capitán</title>
+</head>
+
+<body>
+    <div class="cabecera">
+        <div class="imagen-logo">
+            <img src="../img/itsva.jpg" alt="itsva" style="justify-self: center;">
+        </div>
+    </div>
+
+    <nav id="menu">
+        <ul class="menu">
+           <li><a href="indexAdmin.php"><i></i> Inicio</a></li>
+            <li><a href="#reservar"><i></i>Subconsultas</a></li>
+            <li><a href="#menuRestaurant"><i ></i>Procedimientos Almacenados</a></li>
+            <li><a href="#nosotros"><i></i>Triggers</a></li>
+            <li><a href="productos.php"><i></i>Transacciones</a></li>
+        </ul>
+    </nav>
+
+    <section class="niñasucia" >
+  
+
+
+  </section>
+
+<br><br><br><br><br><br><br><br><br><br><br>
+
+
+    <footer>
+        
+    </footer>
+
+</body>
+
+</html>
